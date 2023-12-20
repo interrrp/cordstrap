2541d6d5b894eedf8f0c3feb232dd1ffa198f134
interrrp <int@addictedto.coffee>
2023-12-20 14:33:40 +0800

    Create Discord client

--- cordstrap/__main__.py
+++ cordstrap/__main__.py
@@ -1,8 +1,17 @@
+from pathlib import Path
+
 import typer
+from pydantic_yaml import parse_yaml_file_as
+
+from cordstrap.discord import DiscordClient
+from cordstrap.template import Template
 
 
-def main_command() -> None:
-    pass
+def main_command(template_path: Path = Path("server.cordstrap.yaml")) -> None:
+    template = parse_yaml_file_as(Template, template_path)
+    client = DiscordClient("ABC")
+    for channel in template.channels:
+        client.create_channel(123, channel)
 
 
 def main() -> None:
